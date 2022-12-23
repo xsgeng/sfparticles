2f5a4fc53e36beadb6dde221ed2fc6a6f6657027
gengxs <xsgeng@siom.ac.cn>
2022-12-23 18:55:46 +0800

    avoid cycled import

--- sfparticles/__init__.py
+++ sfparticles/__init__.py
@@ -1,11 +1,6 @@
-from .particles import Particles, RadiationReactionType
-from .simulation import Simulation
-from .fields import Fields
-
-__all__ = ["Particles", "Simulation", "Fields", "RadiationReactionType"]
-
 from os import environ
 _use_gpu = False
 if "SFPARTICLES_USE_GPU" in environ:
     if environ["SFPARTICLES_USE_GPU"] == "1":
+        print("using GPU")
         _use_gpu = True
