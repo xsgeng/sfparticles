53885cf57b296d3942af18cdd75bdaedd99c9ef8
gengxs <xsgeng@siom.ac.cn>
2024-01-24 14:06:06 +0800

    add focused gaussian laser

--- sfparticles/fields.py
+++ sfparticles/fields.py
@@ -47,6 +47,7 @@ class Fields(object):
 
         self.field_func_inline = field_func_inline
         self.field_func = field_function
+        self.field_func_input = field_func
 
     def __add__(self, other):
         field_func_inline1 = self.field_func_inline
@@ -68,6 +69,7 @@ class Fields(object):
 def simple_laser_pulse(a0, w0, ctau, direction=1,  x0=0, wavelength=0.8e-6, pol_angle=0, cep=0):
     '''
     Simple laser pulse without focusing.
+
     Parameters
     ----------
     a0 : float
@@ -98,8 +100,9 @@ def simple_laser_pulse(a0, w0, ctau, direction=1,  x0=0, wavelength=0.8e-6, pol_
     E0 = a0 / a0_norm
 
     def _laser_pulse(x, y, z, t):
+        x = direction * x
         r2 = y**2 + z**2
-        phi = k0*(x-x0 - direction*c*t)
+        phi = k0*(x - direction*x0 - c*t)
 
         E = E0 * cos(phi + cep) * exp(-r2/w0**2) * exp(-phi**2 / (k0*ctau)**2)
         Ex = 0.0
@@ -117,6 +120,7 @@ def simple_laser_pulse(a0, w0, ctau, direction=1,  x0=0, wavelength=0.8e-6, pol_
 def gaussian_laser_pulse(a0, w0, ctau, direction=1, x0=0.0, l0=0.8e-6, pol_angle=0.0, cep=0.0):
     '''
     Gaussian laser pulse
+
     Parameters
     ----------
     a0 : float
@@ -161,6 +165,101 @@ def gaussian_laser_pulse(a0, w0, ctau, direction=1, x0=0.0, l0=0.8e-6, pol_angle
         Bx = 0.0
         By = -Ez / c * direction
         Bz = Ey / c * direction
+        return (Ex, Ey, Ez, Bx, By, Bz)
+
+    return Fields(_gaussian_pulse)
+
+
+def focused_gaussian_laser_pulse(a0, w0, ctau, direction=1, xfoc=0.0, x0=0.0, l0=0.8e-6, pol_angle=0.0, cep=0.0):
+    '''
+    Focused Gaussian laser pulse
+
+    Ref. Electron Acceleration by a Tightly Focused Laser Beam [10.1103/PhysRevLett.88.095005]
+
+    Parameters
+    ----------
+    a0 : float
+        Amplitude of the laser pulse
+    w0 : float
+        w_0 of the laser pulse at 1/e
+    ctau : float
+        Width of the laser pulse in length at 1/e
+    direction : int
+        Direction of the laser pulse (+1 or -1)
+    xfoc: float
+        The x position of focal plane
+    x0 : float
+        center of the laser pulse
+    l0 : float
+        Wavelength of the laser pulse
+    pol_angle : float
+        Polarization angle of the laser pulse in rad
+    cep : float
+        Carrier envelope phase of the laser pulse
+    Returns
+    -------
+    gaussian pulse Fields object
+    '''
+    assert direction == 1 or direction == -1, "direction must be +1 (+x) or -1 (-x)"
+
+    omega0 = 2*pi*c/l0
+    k0 = 2*pi/l0
+    zR = pi*w0**2 / l0
+    ϵ = w0/zR
+    a0_norm = e / (m_e * c * omega0)
+    E0 = a0 / a0_norm
+    def _gaussian_pulse(x, y, z, t):
+        x = direction * (x - xfoc)
+        r2 = y**2 + z**2
+        wx = w0 * sqrt(1 + (x/zR)**2)
+
+        ψG  = atan(x/zR)
+        ψR = k0*x * r2/2/(x**2 + zR**2)
+        ψP = k0 * (x - direction * x0 - c*t)
+        ψ = ψG + ψR + ψP + cep
+
+        ρ = sqrt(r2) / w0
+        ξ = y/w0
+        ν = z/w0
+        η = 1/sqrt(1 + (x/zR)**2) # w0/w
+
+
+        ρ2 = ρ**2
+        ρ4 = ρ**4
+        ρ6 = ρ**6
+        ρ8 = ρ**8
+        E = E0 * w0/wx*exp(-r2/wx**2) * exp(-(ψ-cep)**2/(k0*ctau)**2)
+
+        S0 = sin(ψ)
+        S2 = η**2 *sin(ψ + 2*ψG)
+        S3 = η**3 *sin(ψ + 3*ψG)
+        S4 = η**4 *sin(ψ + 4*ψG)
+        S5 = η**5 *sin(ψ + 5*ψG)
+        S6 = η**6 *sin(ψ + 6*ψG)
+
+        C1 = η**1 *cos(ψ + 1*ψG)
+        C2 = η**2 *cos(ψ + 2*ψG)
+        C3 = η**3 *cos(ψ + 3*ψG)
+        C4 = η**4 *cos(ψ + 4*ψG)
+        C5 = η**5 *cos(ψ + 5*ψG)
+        C6 = η**6 *cos(ψ + 6*ψG)
+        C7 = η**7 *cos(ψ + 7*ψG)
+
+        Ex_ = E * ξ*(ϵ*C1 + ϵ**3*(-C2/2 + ρ2*C3 - ρ4*C4/4) + ϵ**5*(-3/8*C3 - 3/8*ρ2*C4 + 17/16*ρ4*C5 - 3/8*ρ6*C6 + ρ8*C7/32))
+        Ey_ = E * (S0 + ϵ**2*(ξ**2*S2 - ρ4*S3/4) + ϵ**4*(S2/8-ρ2*S3/4-ρ2*(ρ2-16*ξ**2)*S4/16 - ρ4*(ρ2+2*ξ**2)*S5/8 + ρ8*S6/32))
+        Ez_ = E * ξ*ν*(ϵ**2*S2 + ϵ**4*(ρ2*S4 - ρ4*S5/4))
+
+        Bx_ = E/c * ν*(ϵ*C1 + ϵ**3*(C2/2 + ρ2*C3/2 - ρ4*C4/4) + ϵ**5*(3/8*C3 + 3/8*ρ2*C4 + 3/16*ρ4*C5 - ρ6*C6/4 + ρ8*C7/32))
+        By_ = 0
+        Bz_ = E/c * (S0 + ϵ**2*(ρ2*S2/2 - ρ4*S3/4) + ϵ**4*(-S2/8 + ρ2*S3/4 + 5/16*ρ4*S4 - ρ6*S5/4 + ρ8*S6/32))
+        
+        Ex = Ex_
+        Ey = Ey_ * cos(pol_angle) - Ez_ * sin(pol_angle)
+        Ez = Ey_ * sin(pol_angle) + Ez_ * cos(pol_angle)
+        
+        Bx = Bx_
+        By = (By_ * cos(pol_angle) - Bz_ * sin(pol_angle)) * direction
+        Bz = (By_ * sin(pol_angle) + Bz_ * cos(pol_angle)) * direction
         return (Ex, Ey, Ez, Bx, By, Bz)
 
     return Fields(_gaussian_pulse)
